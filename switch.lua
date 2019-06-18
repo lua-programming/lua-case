5c91a44768513534e4e134c8b63076119d41dd1f
Harry <otgo@outlook.es>
2019-06-18 02:15:25 -0500

    Update switch.lua

--- switch.lua
+++ switch.lua
@@ -1,20 +1,25 @@
 local switch = {}
 local methods = {}
-function methods:case(value, _function_, ...)
+function methods:Case(value, _function_, ...)
     if self.value == value and not self.taken then
-        self.taken = true
         return pcall(_function_, ...)
     end
     return false
 end
-function methods:default(_function_, ...)
+function methods:Default(_function_, ...)
     if not self.taken then
         return pcall(_function_, ...)
     end
     return false
 end
-
-function switch.switch(value)
+function methods:Break()
+    if self.taken == true then
+        return false
+    end
+    self.taken = true
+    return true
+end
+function switch.Switch(value)
     local tab = {__index = methods}
     local self = setmetatable({value = value}, tab)
     return self
